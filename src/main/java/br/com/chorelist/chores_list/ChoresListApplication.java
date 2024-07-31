09371ae40d2d9b7043617df661c64599f7a66fbb
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-07-31 17:26:05 -0300

    init repo

---
+++ src/main/java/br/com/chorelist/chores_list/ChoresListApplication.java
@@ -0,0 +1,13 @@
+package br.com.chorelist.chores_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoresListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoresListApplication.class, args);
+	}
+
+}
